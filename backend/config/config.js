e57cc14887fb0a4cb992e03fb7f02f70f115e2ef
Temitope1606 <alawodetemitope9@gmail.com>
2025-04-01 18:50:56 -0800

    Created .sequelizerc file and made changes to some files

--- backend/config/config.js
+++ backend/config/config.js
@@ -13,8 +13,40 @@ const config = {
     dialect: 'postgres'
   },
   jwtSecret: process.env.JWT_SECRET_KEY,
+  production: {
+    // use_env_variable: process.env.DATABASE_URL, // Tells Sequelize CLI to use DATABASE_URL
+    use_env_variable: "DATABASE_URL", // Tells Sequelize CLI to use DATABASE_URL
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 }
 // Verify that environment variables are correctly loaded by logging them (for debugging)
 console.log(process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_NAME, process.env.DB_HOST, process.env.DB_PORT);
 
 module.exports = config;
+
+module.exports = {
+  development: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+  },
+  production: {
+    use_env_variable: "DATABASE_URL", // Tells Sequelize CLI to use DATABASE_URL
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
