3c2c4088e2c6eb809bd47ce5666682d802693763
Temitope1606 <alawodetemitope9@gmail.com>
2025-04-01 18:59:26 -0800

    Made changes to some files

--- backend/config/config.js
+++ backend/config/config.js
@@ -29,24 +29,3 @@ const config = {
 console.log(process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_NAME, process.env.DB_HOST, process.env.DB_PORT);
 
 module.exports = config;
-
-module.exports = {
-  development: {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-  },
-  production: {
-    use_env_variable: "DATABASE_URL", // Tells Sequelize CLI to use DATABASE_URL
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  },
-};
